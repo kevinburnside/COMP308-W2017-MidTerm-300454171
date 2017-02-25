cef9d74de2b760b3171b3e43dbccbbd8689a1a0b
kevinburnside <kevinburnside15@hotmail.com>
2017-02-25 10:20:25 -0500

    fixed

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
   //local MongoDB deployment ->
-  //"URI": "mongodb://localhost/books308"
+ // "URI": "mongodb://localhost/booksdb"
   //Remote MongoDB deployment ->
-    URI : "mongodb: thomas:123456@ds161059.mlab.com:61059/books308"
+   // URI : "mongodb: thomas:123456@ds161059.mlab.com:61059/books308"
 };
