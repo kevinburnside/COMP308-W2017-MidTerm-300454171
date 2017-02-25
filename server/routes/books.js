f7870aa9770b8d5e15a0397d366ab3baf504d6d2
kevinburnside <kevinburnside15@hotmail.com>
2017-02-25 11:38:37 -0500

    added some auth stuff

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,10 +1,25 @@
+/*Books.js Kevin Burnside 300454171 MidTerm WebApp */
 // modules required for routing
 let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
+let passport = require('passport');
+
+// define the user model
+let UserModel = require('../models/users');
+let User = UserModel.User; // alias for User Model - User object
 
 // define the book model
 let book = require('../models/books');
+
+// create a function to check if the user is authenticated
+function requireAuth(req, res, next) {
+  // check if the user is logged in
+  if(!req.isAuthenticated()) {
+    return res.redirect('/login');
+  }
+  next();
+}
 
 /* GET books List page. READ */
 router.get('/', (req, res, next) => {
@@ -24,15 +39,16 @@ router.get('/', (req, res, next) => {
 });
 
 //  GET the Book Details page in order to add a new Book
-router.get('/add', (req, res, next) => {
+router.get('/add', requireAuth, (req, res, next) => {
   res.render('books/details', {
     title: "Add a new book",
-    books: ''
+    books: '',
+    displayName: req.user.displayName
     });
 });
 
 // POST process the Book Details page and create a new Book - CREATE
-router.post('/add', (req, res, next) => {
+router.post('/add', requireAuth, (req, res, next) => {
 
   let newBook = book({
     "Title": req.body.title,    
@@ -51,7 +67,7 @@ router.post('/add', (req, res, next) => {
   });
 
 // GET the Book Details page in order to edit an existing Book
-router.get('/:id', (req, res, next) => {
+router.get('/:id', requireAuth, (req, res, next) => {
  try {
       // get a reference to the id from the url
       let id = mongoose.Types.ObjectId.createFromHexString(req.params.id);
@@ -65,7 +81,8 @@ router.get('/:id', (req, res, next) => {
           // show the book details view
           res.render('books/details', {
               title: 'Edit Details',
-              books: books            
+              books: books,
+              displayName: req.user.displayName            
           });
         }
       });
@@ -75,7 +92,7 @@ router.get('/:id', (req, res, next) => {
     }
 });
 // POST - process the information passed from the details form and update the document
-router.post('/:id', (req, res, next) => {
+router.post('/:id', requireAuth, (req, res, next) => {
  // get a reference to the id from the url
     let id = req.params.id;
 
@@ -99,7 +116,7 @@ book.update({_id: id}, updatedBook, (err) => {
 });
 
 // GET - process the delete by user id
-router.get('/delete/:id', (req, res, next) => {
+router.get('/delete/:id',requireAuth, (req, res, next) => {
 let id = req.params.id;
 
     book.remove({_id: id}, (err) => {
